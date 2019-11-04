b016fb7c3f6f22c92b03a9f53fa5fe72126a4305
Italo Leandro <leandroitalo433@gmail.com>
2019-11-04 01:24:50 -0300

    Adicionando frontend

---
+++ frontend/src/pages/New/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import camera from '../../assets/camera.svg';
+import './styles.css';
+
+export default function New() {
+
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <label id="thumbnail">
+                <input type="file" />
+                <img src={camera} alt="Select img"/>
+            </label>
+
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
+            <input
+                id="price"
+                placeholder="Valor cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className="btn">Cadastrar</button>
+        </form>
+    );
+}
