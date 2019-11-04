a0e8ecca864f6f1ce91b37832caf5b5aceb6a1eb
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-11-04 02:08:26 -0300

    Frontend finalizado

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 
 import camera from '../../assets/camera.svg';
 import './styles.css';
 
-export default function New() {
+export default function New({ history }) {
 
+    const [thumbnail, setThumbnail] = useState();
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
 
-    function handleSubmit() {
+    const preview = useMemo(
+        () => {
+            return thumbnail ? URL.createObjectURL(thumbnail) : null;
+        }, [thumbnail]
+    );
 
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        });
+
+        history.push('/dashboard');
     }
 
     return (
         <form onSubmit={handleSubmit}>
-
-            <label id="thumbnail">
-                <input type="file" />
-                <img src={camera} alt="Select img"/>
+            <label
+                id="thumbnail"
+                style={{ backgroundImage: `url(${preview})` }}
+                className={thumbnail ? 'has-thumbnail' : null}
+            >
+                <input
+                    type="file"
+                    onChange={event => setThumbnail(event.target.files[0])}
+                />
+                <img src={camera} alt="Select img" />
             </label>
 
             <label htmlFor="company">EMPRESA *</label>
