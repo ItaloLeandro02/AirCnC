44c99e0e364cc894f579744bceb80b7f2a510349
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-11-05 20:06:28 -0300

    Aplicaçao mobile finalizada

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -19,8 +19,8 @@ function SpotList({ tech, navigation }) {
         loadSpots();
     }, []);
 
-    function handleNavigate() {
-        navigation.navigate('Book');
+    function handleNavigate(id) {
+        navigation.navigate('Book', { id });
     }
 
     return (
@@ -35,10 +35,10 @@ function SpotList({ tech, navigation }) {
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
                     <View style={styles.listItem}>
-                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }}/>
                         <Text style={styles.company}>{item.company}</Text>
                         <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={handleNavigate} style={styles.button}>
+                        <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
                             <Text style={styles.buttonText}>Solicitar reserva</Text>
                         </TouchableOpacity>
                     </View>
@@ -73,13 +73,13 @@ const styles = StyleSheet.create({
     },
 
     thumbnail: {
-        width: 200,
-        height: 120,
+        width: 280,
+        height: 168,
         resizeMode: 'cover',
-        borderRadius: 2,
+        borderRadius: 2
     },
 
-    company: {
+    company: { 
         fontSize: 24,
         fontWeight: 'bold',
         color: '#333',
