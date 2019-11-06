556489d38cd827f730eca3990b3c7169b3bf520d
ItaloLeandro02 <leandroitalo433@gmail.com>
2019-11-05 22:22:11 -0300

    Semana OmniStack 9.0 finalizada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const multer  = require('multer');
+const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -18,5 +20,8 @@ routes.post('/v1/spots', upload.single('thumbnail'), SpotController.store)
 routes.get('/v1/dashboard', DashboardController.show);
 
 routes.post('/v1/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/v1/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/v1/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
